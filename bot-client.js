34e564015303c9a899873d96798e44562bbeff1c
NzsXc <nakashin081011@gmail.com>
2026-09-25 23:31:02 +0900

    Update bot-client.js

--- bot-client.js
+++ bot-client.js
@@ -1,4 +1,3 @@
-
 let botMatch=false,botThinking=false,botKnowledge=null,botWorker=null,botEpoch=0,botTurn=0;
 let randomSearchBusy=false,randomSearchCancelled=false;
 function stopBot(){botEpoch++;botWorker?.terminate();botWorker=null;botThinking=false;botMatch=false;}
